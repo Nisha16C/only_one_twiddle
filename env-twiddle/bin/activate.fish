36c2925effce3ea2b813798c110cbcbc67dfc0f5
user <user@twiddle.os3.com>
2023-09-04 06:57:37 +0000

    only-chnges

--- env-twiddle/bin/activate.fish
+++ env-twiddle/bin/activate.fish
@@ -29,7 +29,7 @@ end
 # unset irrelevant variables
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/home/ubuntu/twitter-clone-app/env-tweet"
+set -gx VIRTUAL_ENV "/home/user/twitter-clone-app/env-twiddle"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
@@ -52,8 +52,8 @@ if test -z "$VIRTUAL_ENV_DISABLE_PROMPT"
         set -l old_status $status
 
         # Prompt override?
-        if test -n "(env-tweet) "
-            printf "%s%s" "(env-tweet) " (set_color normal)
+        if test -n "(env-twiddle) "
+            printf "%s%s" "(env-twiddle) " (set_color normal)
         else
             # ...Otherwise, prepend env
             set -l _checkbase (basename "$VIRTUAL_ENV")
